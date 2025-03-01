7537d9bddeee6152908a7d98a73c7ca888afdf33
Josh Pigford <josh@joshpigford.com>
2025-03-01 17:08:11 -0500

    Add Session Resume to Static Pages and Update Home View
    - Add `resume_session_if_available` method to PagesController to ensure Current.user is set
    - Modify home page view to replace "My Artists" and "Create New Artist" with "Dashboard" link
    - Simplify home page navigation for authenticated users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   # Skip authentication for static pages
   allow_unauthenticated_access
 
+  before_action :resume_session_if_available
+
   def home
   end
 
@@ -12,5 +14,12 @@ class PagesController < ApplicationController
   end
 
   def terms
+  end
+
+  private
+
+  def resume_session_if_available
+    # Ensures Current.user is set if a valid session exists, even though we don't require authentication
+    authenticated?
   end
 end
